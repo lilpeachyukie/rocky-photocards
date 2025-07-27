e1b7316abf814d468a52e05ff582303cec6b7863
Lilpeachyukie <haifunskhid95@gmail.com>
2025-07-27 20:08:31 +0800

    deploy

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\rocky-photocards\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\ROCKY\rocky-photocards\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
