14959d1638ae16dfa8daadab821632765b4e8d75
Fakhira Iwani <wani_fakhira95@yahoo.com>
2025-07-27 19:18:12 +0800

    fixing mobile screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
   update = '250727';
 
   albumPC = [
-    { caption: 'ROCKYST: Classic Ver', total: 9 },
-    { caption: 'ROCKYST: Modern Ver', total: 9 },
-    { caption: 'ROCKYST: Platform Ver - random', total: 9 },
-    { caption: 'ROCKYST: Platform Ver - set', total: 8 },
-    { caption: 'ROCKYST: Platform Ver- qr', total: 1 },
-    { caption: 'BLANK: Burgundy Ver', total: 6 },
-    { caption: 'BLANK: White Ver', total: 6 },
+    { caption: 'ROCKYST: Classic', total: 9 },
+    { caption: 'ROCKYST: Modern', total: 9 },
+    { caption: 'ROCKYST: Platform - random', total: 9 },
+    { caption: 'ROCKYST: Platform - set', total: 8 },
+    { caption: 'ROCKYST: Platform - qr', total: 1 },
+    { caption: 'BLANK: Burgundy', total: 6 },
+    { caption: 'BLANK: White', total: 6 },
 
   ];
 
@@ -54,7 +54,7 @@ export class AppComponent {
     { caption: 'Stargraphy: Purchased >50k won Offline', total: 1 },
     { caption: 'Stargraphy: Purchased >50k won Online', total: 1 },
     { caption: 'Stargraphy - Japan MD : Tin Case', total: 1 },
-    { caption: 'Stargraphy - Japan MD : Photopack', total: 2 },
+    { caption: 'Stargraphy - Japan MD : Photopack', total: 1 },
     { caption: 'Stargraphy - Exhibition : MD', total: 3 },
     { caption: 'Stargraphy - Exhibition : Purchased >70k won', total: 1 },
     { caption: 'K-POP ANDEAN FS Colombia', total: 1},
