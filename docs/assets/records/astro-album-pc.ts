04093c4e870fede79b80d89713660b236d06cb12
Fakhira Iwani <wani_fakhira95@yahoo.com>
2025-07-30 00:52:07 +0800

    add astro album pc

---
+++ docs/assets/records/astro-album-pc.ts
@@ -0,0 +1,33 @@
+export var astroAlbumPC = [
+  { caption: 'Spring up', total: 2 },
+	{ caption: 'Summer Vibes', total: 3 },
+	{ caption: 'Autumn Story - Red', total: 2 },
+	{ caption: 'Autumn Story - Orange', total: 2 },
+	{ caption: 'Winter Dream', total: 2 },
+	{ caption: 'Dream Pt1 - Day', total: 3 },
+	{ caption: 'Dream Pt1 - Night', total: 3 },
+	{ caption: 'Dream Pt2 - Wind', total: 3 },
+	{ caption: 'Dream Pt2 - Wish', total: 3 },
+	{ caption: 'Dream Sp. Edition', total: 2 },
+	{ caption: 'Rise up', total: 4 },
+	{ caption: 'All Light - White', total: 4 },
+	{ caption: 'All Light - Green', total: 4 },
+	{ caption: 'Blue Flame - Book', total: 2 },
+	{ caption: 'Blue Flame - Story', total: 2 },
+	{ caption: 'One & Only', total: 2 },
+	{ caption: 'Gateway - Another World', total: 2 },
+	{ caption: 'Gateway - Time Traveler', total: 2 },
+	{ caption: 'All Yours - You', total: 2 },
+	{ caption: 'All Yours - Me', total: 2 },
+	{ caption: 'All Yours - Us', total: 2 },
+	{ caption: 'Switch On', total: 8 },
+	{ caption: 'Restore - Staycation', total: 4 },
+	{ caption: 'Restore - Vacation', total: 4 },
+	{ caption: 'Drive to the Starry Road - Drive', total: 4 },
+	{ caption: 'Drive to the Starry Road - Starry', total: 4 },
+	{ caption: 'Drive to the Starry Road - Road', total: 4 },
+]
+
+
+export var astroNonAlbum = {}
+///Kihno Sp - move to non-album
